de42690db2b71bdeb86c6fdb7dc6c7edc569b9be
Frank Zimmermann <frank.zimmermann@nordakademie.de>
2020-09-29 16:51:45 +0200

    All that stuff

---
+++ primes/src/test/java/primes/TestPrime.java
@@ -0,0 +1,27 @@
+package primes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import de.nordakademie.primes.PrimeTester;
+
+public class TestPrime {
+
+	private PrimeTester out = new PrimeTester();
+	@Test
+	public void test() {
+		assertFalse(out.isPrime(1));
+	}
+	
+	@Test
+	public void test2() {
+		assertTrue(out.isPrime(2));
+	}
+	@Test
+	public void test4() {
+		assertFalse(out.isPrime(4));
+	}
+
+
+}
