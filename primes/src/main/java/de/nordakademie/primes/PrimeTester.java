461566a8515e829860c7b48d1cef452570e3e1f5
Frank Zimmermann <frank.zimmermann@nordakademie.de>
2020-10-12 16:58:50 +0200

    A commit Message

--- primes/src/main/java/de/nordakademie/primes/PrimeTester.java
+++ primes/src/main/java/de/nordakademie/primes/PrimeTester.java
@@ -1,7 +1,24 @@
 package de.nordakademie.primes;
 
-public class PrimeTester {
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
 
+public class PrimeTester {
+	
+	public static void main(String[] args) {
+		try (	OutputStream os = new FileOutputStream("out.txt");
+				Writer w = new OutputStreamWriter(os, StandardCharsets.UTF_8.name());
+				BufferedWriter bw = new BufferedWriter(w)) {
+				bw.write("hallo welt");
+				bw.write("\n");
+			} catch (Exception e) {
+				System.err.println("Fehler beim Schreiben der Datei out.txt");
+			}
+		} 
 	public boolean  isPrime(int n) {
 		if (n<=1) {
 			return false;
