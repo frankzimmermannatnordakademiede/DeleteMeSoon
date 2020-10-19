f139184e0f473bf3cd9ffd6057b14d5c45fb963f
frankzimmermannatnordakademiede <frank.zimmermann@nordakademie.de>
2020-10-19 12:21:53 +0000

    Formatted correctly

--- primes/src/test/java/primes/TestPrime.java
+++ primes/src/test/java/primes/TestPrime.java
@@ -8,29 +8,31 @@ import de.nordakademie.primes.PrimeTester;
 
 public class TestPrime {
 
-	private PrimeTester out = new PrimeTester();
-	@Test
-	public void test() {
-		assertFalse(out.isPrime(1));
-	}
-	
-	@Test
-	public void test2() {
-		assertTrue(out.isPrime(2));
-	}
-	@Test
-	public void test4() {
-		assertFalse(out.isPrime(4));
-	}
-		@Test
-	public void test63() {
-		assertFalse(out.isPrime(63));
-	}
-	
-	@Test
-	public void test67(){
-	assertTrue(out.isPrime(67));
-	}
+    private PrimeTester out = new PrimeTester();
 
+    @Test
+    public void test() {
+        assertFalse(out.isPrime(1));
+    }
+
+    @Test
+    public void test2() {
+        assertTrue(out.isPrime(2));
+    }
+
+    @Test
+    public void test4() {
+        assertFalse(out.isPrime(4));
+    }
+
+    @Test
+    public void test63() {
+        assertFalse(out.isPrime(63));
+    }
+
+    @Test
+    public void test67() {
+        assertTrue(out.isPrime(67));
+    }
 
 }
